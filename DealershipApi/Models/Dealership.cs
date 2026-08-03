713b757835e56030eeec2081b961ea7ff0f4a51a
VagelisMerziotis <vmerziot1997@gmail.com>
2026-08-03 20:52:35 +0300

    Created Models for tables in DB

---
+++ DealershipApi/Models/Dealership.cs
@@ -0,0 +1,22 @@
+namespace DealershipApi.Models;
+
+public class Dealership
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Number { get; set; }
+    public int Zipcode { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Website { get; set; }
+    public decimal SqFeet { get; set; }
+    public decimal Budget { get; set; }
+    public DateTime Created { get; init; }
+    public DateTime Modified { get; set; }
+    public List<User> Users { get; set; } = new();
+    public List<Vehicle> Vehicles { get; set; } = new();
+}
---
+++ DealershipApi/Models/User.cs
@@ -0,0 +1,20 @@
+namespace DealershipApi.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public int DealershipId { get; set; }
+    public Dealership Dealership { get; set; }
+    public string Role { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public int Salary { get; set; }
+    public bool IsStakeholder { get; set; }
+}
