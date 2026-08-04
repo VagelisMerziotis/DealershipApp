a42f79f2a8af16955c34a3521654d46dfb9eda82
VagelisMerziotis <vmerziot1997@gmail.com>
2026-08-04 15:27:13 +0300

    Add Constructor comment

--- DealershipApi/Data/AppDbContext.cs
+++ DealershipApi/Data/AppDbContext.cs
@@ -5,6 +5,7 @@ namespace DealershipApi.Data;
 
 public class AppDbContext : DbContext
 {
+    // Constructor
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
     
     public DbSet<User> Users { get; set; }
