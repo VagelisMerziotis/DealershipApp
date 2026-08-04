7989ff145b766bb0de6c8391857c3d1142ce97f5
VagelisMerziotis <vmerziot1997@gmail.com>
2026-08-04 16:52:50 +0300

    Use the DB initializer in custom scope once

--- DealershipApi/Program.cs
+++ DealershipApi/Program.cs
@@ -1,6 +1,5 @@
 using DealershipApi.Data;
 using Microsoft.EntityFrameworkCore;
-using DealershipApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +14,12 @@ builder.Services.AddDbContext<AppDbContext>((options) =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    DbInitializer.Seed(db);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -23,28 +28,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
