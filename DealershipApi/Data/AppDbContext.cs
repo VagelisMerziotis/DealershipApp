6b28342757c278eedce1e36dcfeccf34a1387ccb
VagelisMerziotis <vmerziot1997@gmail.com>
2026-08-12 17:22:40 +0300

    Create foreign keys and cascade policy for Users and Vehicles in DB

--- DealershipApi/Data/AppDbContext.cs
+++ DealershipApi/Data/AppDbContext.cs
@@ -17,5 +17,19 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<Vehicle>()
             .HasDiscriminator<string>("VehicleType")
             .HasValue<Automobile>("Automobile");
+        
+        //Create foreign key and cascade policy with dealership 
+        modelBuilder.Entity<Vehicle>()
+            .HasOne(v => v.Dealership)
+            .WithMany(d => d.Vehicles)
+            .HasForeignKey(v => v.DealershipId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Create foreign key and cascade with dealership
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Dealership)
+            .WithMany(d => d.Users)
+            .HasForeignKey(u => u.DealershipId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
